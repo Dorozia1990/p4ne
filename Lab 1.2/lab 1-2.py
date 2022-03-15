267014e2413562fe74a1ca1ab8dfd0507a143557
Dorozia1990 <dorozia1990@gmail.com>
2022-03-15 18:30:24 +0300

    second lab (style)

--- Lab 1.2/lab 1-2.py
+++ Lab 1.2/lab 1-2.py
@@ -1,7 +1,8 @@
+#imports
 from matplotlib import pyplot
 from openpyxl import load_workbook
-#declaration
 
+#declaration
 #Get value function
 def getvalue(x):
     return x.value
@@ -13,6 +14,8 @@ sheet = wb['Data']
 years = list(map(getvalue, sheet['A'][1:]))
 temperature = list(map(getvalue, sheet['C'][1:]))
 sunActivity = list(map(getvalue, sheet['D'][1:]))
+
 pyplot.plot(years, temperature, label="Отн. температура")
 pyplot.plot(years, sunActivity, label="Акт. Солнца")
+
 pyplot.show()
