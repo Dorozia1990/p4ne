d3df0c7ed428f01c411c68b88ad4fc71565b5cb2
Dorozia1990 <dorozia1990@gmail.com>
2022-03-16 18:57:36 +0300

    third lab (removed magic numbers where possible)

--- Lab 1.3/lab 1-3.py
+++ Lab 1.3/lab 1-3.py
@@ -6,6 +6,7 @@ snmp_object = ObjectIdentity('1.3.6.1.2.1.2.2.1.2')
 snmp_ver = ObjectIdentity('SNMPv2-MIB', 'sysDescr', 0)
 ipaddr_string = '10.31.70.107'
 port_int = 161
+varBindsIndex = 3
 
 result = getCmd(
 	SnmpEngine(),
@@ -14,6 +15,7 @@ result = getCmd(
 	ContextData(),
 	ObjectType(snmp_ver)
 )
+#result: (errorIndication, errorStatus, errorIndex, varBinds)
 
 result2 = nextCmd(
 	SnmpEngine(),
@@ -23,13 +25,14 @@ result2 = nextCmd(
 	ObjectType(snmp_object),
 	lexicographicMode=False
 )
+#result2: (errorIndication, errorStatus, errorIndex, varBinds)
 
 #executable
 
 for i in result:
-	for j in i[3]:
+	for j in i[varBindsIndex]:
 		print(j)
 
 for i in result2:
-	for j in i[3]:
+	for j in i[varBindsIndex]:
 		print(j)
