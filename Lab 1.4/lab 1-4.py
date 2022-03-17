8a7258867379afb070081f95b705370979f44063
Dorozia1990 <dorozia1990@gmail.com>
2022-03-17 18:07:33 +0300

    fourth lab (style)

--- Lab 1.4/lab 1-4.py
+++ Lab 1.4/lab 1-4.py
@@ -1,24 +1,27 @@
-#imports
+# imports
 
 from ipaddress import IPv4Network
 import random
 
-#defs
+# definitions
+
 
 class IPv4RandomNetwork(IPv4Network):
     def __init__(self):
         self.network_address = random.randint(0x0B000000, 0xDF000000)
         self.netmask = random.randint(8, 24)
-        IPv4Network.__init__(self, (self.network_address, self.netmask), strict = 0)
+        IPv4Network.__init__(self, (self.network_address, self.netmask), strict=False)
+
     def key_value(self):
         return int(self.network_address)+int(self.netmask)*2**32
 
-#executable
 
-def keyFunc(addr):
+def keyfunc(addr):
     return addr.key_value()
 
-n = IPv4RandomNetwork()
+# executable
+
+
 ipList = []
 i = 0
 while i < 50:
@@ -27,7 +30,7 @@ while i < 50:
         ipList.append(tempNetwork)
         i += 1
 
-newIpNetList = sorted(ipList, key = keyFunc)
+newIpNetList = sorted(ipList, key=keyfunc)
 
 for ipNet in newIpNetList:
     print(ipNet)
